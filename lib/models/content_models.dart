62a815d5e5aa8d260b02059bff9cc80811c6e89e
adinanhamza <adinanck4557@gmail.com>
2025-03-26 15:36:15 +0530

    model added

--- lib/models/content_models.dart
+++ lib/models/content_models.dart
@@ -1,4 +1,27 @@
 class Moviemodel{
+String? name;
+String? orginal;
+String?title;
+String? overview;
+String? backdroppath;
+String? posterpath;
+String?  type;
 
+Moviemodel({required this.name,required this.orginal,required this.backdroppath,required this.overview,required this.posterpath,required this.title,required this.type});
   
+
+  factory Moviemodel.fromjson(Map<String,dynamic>json){
+    return Moviemodel(
+      
+      backdroppath: json['backdrop_path'] ?? 'no data',
+      type: json['media_type'] ??   'no data',
+      orginal: json['original_name'] ??  'no data',
+      overview: json[ 'overview']  ??  'no data',
+      name: json[ 'name'] ?? 'no data',
+      posterpath: json['poster_path'] ?? 'no data',
+      title: json['title'] ?? 'no data',
+      
+      
+      );
+  }
 }
