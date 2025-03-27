41d597a3f0d6ccaa08cc17febab12bd58e6180be
adinanhamza <adinanck4557@gmail.com>
2025-03-27 19:58:11 +0530

    created controller

--- lib/services/movieservice.dart
+++ lib/services/movieservice.dart
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
+
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
 import 'package:netflix_clone/constants/apiconstants.dart';
+import 'package:netflix_clone/models/content_models.dart';
 
 class Movieservice {
 
@@ -7,12 +11,23 @@ class Movieservice {
   final topratedmovies = Apiconstants.getTopRatedMovies();
   final trendingmovies = Apiconstants.getTrendingMovies();
   final tvshows = Apiconstants.getTvShows();
-  final searchMovie = Apiconstants.searchMovie();
+  final searchMovie = Apiconstants.searchMovie(); 
 
+  Dio dio = Dio();
 
+Future<List<Moviemodel>>  popularmovie()async{
+try {
+  final response = await dio.get(popularmovies);
+  if(response.data == 200){
+    log('request successfully ${response.statusMessage  }');
+    return (response.data as List).map((movies)=> Moviemodel.fromjson(movies)).toList();
+  }else{
+   log('error : ${response.statusCode} - ${response.statusMessage}');
+  }
+} catch (e) {
+  throw Exception('error :$e');
+}
+return [];
+}
   
-=======
-class Movieservice {
-  
->>>>>>> d72e3fc4e4266a8384cf248828c420cb08ce13ec
 }
